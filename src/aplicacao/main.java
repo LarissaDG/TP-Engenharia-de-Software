8ea094d486872c5cd19c4ea37be497e10a4882bc
Larissa <laladg18@gmail.com>
2020-10-25 16:27:07 -0300

    Escopo do codigo no main. Comentário das chamadas

--- src/aplicacao/main.java
+++ src/aplicacao/main.java
@@ -1,7 +1,20 @@
 package aplicacao;
 
 public class main {
+	//Chama o menu principal
 	
+	//Se a pessoa digitar 1 vai pro menu de login
+	
+	//Uma vez logada a pessoa pode:
+		//1. Cadastrar um jogo
+		//2. Procurar um usuário (lista/arquivo)
+		//3. Procurar um jogo 
+			//3.1 Pegar um jogo emprestado
+				//Listar os jogos(resultado da busca?)
+				//Escolher o jogo pelo indice da lista
+				//Mudar o status do jogo.
+	
+	//Se a pessoa digitar 2 vai pro menu de Cadastro de Usuário
 	 
 
 }
